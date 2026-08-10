b048d0df0e7d4a275d1e0ab5cc109ff135fbb076
Emilie Brandt <emiliebrandt@gmx.de>
2026-08-10 10:34:04 +0200

    create input button + function syntax

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -2,11 +2,15 @@ import "./Color.css";
 
 export default function Color({ color }) {
   const { role, hex, contrastText } = color;
+
+  function handleDeleteButton() {}
+
   return (
     <article className="color-card" style={{ backgroundColor: hex }}>
       <h2 className="color-card-headline">{hex}</h2>
       <p style={{ color: contrastText }}>{role}</p>
       <p style={{ color: contrastText }}> contrast: {contrastText}</p>
+      <button type="button">Delete Color</button>
     </article>
   );
 }
