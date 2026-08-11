88ea0ca9e5f7f17194ca8de61ad41d7046cfb90a
Emilie Brandt <emiliebrandt@gmx.de>
2026-08-11 12:33:22 +0200

    add useStates for the themes and find/filter Theme and Colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,30 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
+import { initialThemes } from "./lib/themes";
+import { useState } from "react";
 
 function App() {
+  // –––––––––––––––––––––– UseStates –––––––––––––––––––––––––
+
   // initialColor is going to change, Theme is now saved to localStorage
   const [listColors, setListColors] = useLocalStorageState("listColors", {
     defaultValue: initialColors,
   });
+
+  // List of all Themes
+  const [themes, setThemes] = useLocalStorageState("themes", {
+    defaultValue: initialThemes,
+  });
+
+  // Current Theme
+  const [activeThemeId, setActiveThemeId] = useState(initialThemes[0].id);
+
+  // –––––––––––––––––––––– Array-Methodes to find active Theme –––––––––––––––––––––––––
+
+  const activeTheme = themes.find((theme) => theme.id === activeThemeId);
+
+  // –––––––––––––––––––––– handle-functions –––––––––––––––––––––––––
 
   // New Color are added to listColor(initialColor)
   function handleAddColor(newColor) {
